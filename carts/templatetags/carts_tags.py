7c2aca47f097d5d4e9cabdf90818023aeafa491d
olg <htsolg@gmail.com>
2025-10-23 23:07:03 +0200

    Add to cart ajax request and json response

--- carts/templatetags/carts_tags.py
+++ carts/templatetags/carts_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from carts.models import Cart
+# from carts.utils import get_user_carts
 
 register = template.Library()
 
@@ -13,8 +14,3 @@ def user_carts(context):
     return Cart.objects.none()
 
 
-# @register.simple_tag()
-# def user_carts(request):    
-#     if request.user.is_authenticated:
-#         return Cart.objects.filter(user=request.user)
-#     return Cart.objects.none()
